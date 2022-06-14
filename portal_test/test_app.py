9964b5c520001bd0665c583a48f5c3997661d390
studio-nakaji <nnnn00223399@gmail.com>
2022-06-14 21:54:47 +0900

    リンクを追加

--- portal_test/test_app.py
+++ portal_test/test_app.py
@@ -3,6 +3,8 @@ from streamlit.cli import main
 
 def main():
     st.write("STRIPEテスト")
+    link = "[決済](https://buy.stripe.com/14k29ngkg9GT0tW3cc)"
+    st.markdown(link,unsafe_allow_html=True)
 
 
 
