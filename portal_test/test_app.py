ad918082c3e2068164a0fbe1de05fffbef321a04
studio-nakaji <nnnn00223399@gmail.com>
2022-06-14 22:25:55 +0900

    リンクをiframeで追加

--- portal_test/test_app.py
+++ portal_test/test_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from streamlit.cli import main
+import streamlit.components.v1 as components
 
 def main():
     st.write("STRIPEテスト")
-    link = "[決済](https://buy.stripe.com/14k29ngkg9GT0tW3cc)"
-    st.markdown(link,unsafe_allow_html=True)
+    components.iframe("https://buy.stripe.com/14k29ngkg9GT0tW3cc")
+    # link = "[決済](https://buy.stripe.com/14k29ngkg9GT0tW3cc)"
+    # st.markdown(link,unsafe_allow_html=True)
 
 
 
